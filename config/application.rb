1eb31b7f9c021b2ab2275d45341ece811d6d97de
Emily V <emilyrobynvirnig@gmail.com>
2011-03-06 19:01:34 -0800

    User signup complete. Done through chapter 8.5 with this commit

--- config/application.rb
+++ config/application.rb
@@ -46,5 +46,7 @@ module SimpleApp2
           ActiveSupport::Dependencies.mechanism = :load
       end
     end
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
   end
 end
