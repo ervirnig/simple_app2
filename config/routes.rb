4dfc12d56c57e9203a45fbb6c56d4760fc73fba3
Emily V <emilyrobynvirnig@gmail.com>
2011-04-19 09:26:49 -0700

    Done with chapter 9, not including exercises

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SimpleApp2::Application.routes.draw do
-  #get "users/new"
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  get "sessions/new"
   resources :users
 
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
